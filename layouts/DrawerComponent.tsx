1c9e9eadfc39a6908e400c7c9398ef806ec026e6
Aryan Jain <aryanjn807@gmail.com>
2022-08-27 14:09:33 +0530

    -> reset

--- layouts/DrawerComponent.tsx
+++ layouts/DrawerComponent.tsx
@@ -44,7 +44,7 @@ export const DrawerHeader = styled("div")(({ theme }) => ({
   alignItems: "center",
   justifyContent: "flex-end",
   padding: theme.spacing(0, 1),
-  // necessary for content to be below app bar
+  //  necessary for content to be below app bar
   ...theme.mixins.toolbar,
 }));
 
